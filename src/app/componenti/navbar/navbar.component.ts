2ffbde61a8d998498ac674437c65dbebf304f64c
gian <gianmarco.clu23@gmail.com>
2024-08-11 18:23:13 +0200

    feat(navbar): implemented share function

--- src/app/componenti/navbar/navbar.component.ts
+++ src/app/componenti/navbar/navbar.component.ts
@@ -1,16 +1,33 @@
 import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatMenuModule} from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [MatIconModule, MatButtonModule, MatToolbarModule, MatMenuModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
-
+  share() {
+    if (navigator.share) {
+      navigator
+        .share({
+          title: 'Segui Santa Lucia',
+          text: 'Descrizione della piattaforma',
+          url: 'https://www.tuapiattaforma.com',
+        })
+        .then(() => {
+          console.log('Condivisione avvenuta con successo');
+        })
+        .catch((error) => {
+          console.error('Errore nella condivisione', error);
+        });
+    } else {
+      console.log('API Web Share non supportata nel browser');
+    }
+  }
 }
