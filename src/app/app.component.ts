8c208f7e14e9a44fbb6732a98d1666852c4061e9
gian <gianmarco.clu23@gmail.com>
2024-08-11 17:18:49 +0200

    feat(app): implemented NavbarComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TabComponent } from './componenti/tab/tab.component';
 import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from './componenti/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent],
+  imports: [HomeComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
