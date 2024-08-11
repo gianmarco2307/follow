ca30e966fcb97f18dd5069295bb17b3811511da9
gian <gianmarco.clu23@gmail.com>
2024-08-11 22:49:59 +0200

    feat(navbar): modified navbar according primeng

--- src/app/componenti/navbar/navbar.component.ts
+++ src/app/componenti/navbar/navbar.component.ts
@@ -1,13 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
+import { BadgeModule } from 'primeng/badge';
+import { AvatarModule } from 'primeng/avatar';
+import { InputTextModule } from 'primeng/inputtext';
+import { CommonModule } from '@angular/common';
+import { RippleModule } from 'primeng/ripple';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [
+    MenubarModule,
+    BadgeModule,
+    AvatarModule,
+    InputTextModule,
+    RippleModule,
+    CommonModule,
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  items: MenuItem[] | undefined;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Segui Santa Lucia',
+        icon: 'pi pi-map-marker',
+        url: '/home',
+      },
+      {
+        label: 'Contatti',
+        icon: 'pi pi-address-book',
+        url: '/contatti',
+      },
+      {
+        label: 'Codividi',
+        icon: 'pi pi-share-alt',
+      },
+    ];
+  }
+
+  navigate(url?: string) {
+    this.router.navigate([url]);
+  }
+  
   share() {
     if (navigator.share) {
       navigator
