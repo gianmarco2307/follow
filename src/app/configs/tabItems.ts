2afb201694f927310d69c7e08a2156ce72fb129b
gian <gianmarco.clu23@gmail.com>
2024-08-11 16:45:33 +0200

    feat(tabItems): added ts file with tabItems

---
+++ src/app/configs/tabItems.ts
@@ -0,0 +1,11 @@
+export interface TabItem {
+    label: string;
+    value: string;
+}
+
+export const tabItems: TabItem[] = [
+  { label: '24-sera', value: 'Sabato Sera' },
+  { label: '25-mattina', value: 'Domenica Mattina' },
+  { label: '25-sera', value: 'Domenica Sera' },
+  { label: '26-sera', value: 'Lunedì Sera' },
+];
