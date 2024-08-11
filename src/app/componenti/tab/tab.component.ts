3fda056ca45a99e134b301f93b4d3aadc2af0a58
gian <gianmarco.clu23@gmail.com>
2024-08-11 22:11:48 +0200

    feat(tab): modified tab accordring primeng

--- src/app/componenti/tab/tab.component.ts
+++ src/app/componenti/tab/tab.component.ts
@@ -1,19 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { TabItem, tabItems } from '../../configs/tabItems';
 import { NgForOf } from '@angular/common';
-
+import { TabMenuModule } from 'primeng/tabmenu';
 
 @Component({
   selector: 'app-tab',
   standalone: true,
-  imports: [NgForOf],
+  imports: [NgForOf, TabMenuModule],
   templateUrl: './tab.component.html',
-  styleUrl: './tab.component.css'
+  styleUrl: './tab.component.css',
 })
 export class TabComponent implements OnInit {
   protected tabs: TabItem[] = [];
+  protected activeItem!: TabItem;
 
   ngOnInit(): void {
     this.tabs = tabItems;
+    this.activeItem = this.tabs[0];
+  }
+
+  onActiveItemChange(event: any) {
+    console.log(event);
+    this.activeItem = event;
   }
 }
--- src/app/configs/tabItems.ts
+++ src/app/configs/tabItems.ts
@@ -1,11 +1,11 @@
 export interface TabItem {
     label: string;
-    value: string;
+    option: string;
 }
 
 export const tabItems: TabItem[] = [
-  { label: '24-sera', value: 'Sabato Sera' },
-  { label: '25-mattina', value: 'Domenica Mattina' },
-  { label: '25-sera', value: 'Domenica Sera' },
-  { label: '26-sera', value: 'Lunedì Sera' },
+  { label: 'Sabato Sera', option: '24-sera' },
+  { label: 'Domenica Mattina', option: '25-mattina' },
+  { label: 'Domenica Sera', option: '25-sera' },
+  { label: 'Lunedì Sera', option: '26-sera' },
 ];
