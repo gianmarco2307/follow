701603ebb4df90c885932e7a175f292f289bdfd2
gian <gianmarco.clu23@gmail.com>
2024-08-12 16:17:47 +0200

    feat(login): modified style in login page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -3,11 +3,15 @@ import { Component } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthenticationService } from '../../servizi/authentication.service';
+import { InputTextModule } from 'primeng/inputtext';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, InputTextModule, FloatLabelModule, PasswordModule, ButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
@@ -20,7 +24,7 @@ export class LoginComponent {
   ) {}
 
   form = this.fb.nonNullable.group({
-    email: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
     password: ['', Validators.required],
   });
 
