9b0a5b68135396312adea350474e3cbe362f90d0
gian <gianmarco.clu23@gmail.com>
2024-08-15 19:41:01 +0200

    feat(routes): added canActivate in manage route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { ManageComponent } from './pages/manage/manage.component';
+import { authGuard } from './guardie/auth.guard';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent},
     {path: 'login', component: LoginComponent},
-    {path: 'manage', component: ManageComponent}
+    {path: 'manage', component: ManageComponent, canActivate: [authGuard]}
 ];
