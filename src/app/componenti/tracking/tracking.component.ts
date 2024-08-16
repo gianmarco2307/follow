dada58872350d4f645d9bef6a994b705b00b2253
gian <gianmarco.clu23@gmail.com>
2024-08-16 10:43:58 +0200

    fix(tracking): fixed dynamic route

--- src/app/componenti/tracking/tracking.component.ts
+++ src/app/componenti/tracking/tracking.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, DoCheck, Input, OnDestroy, OnInit, signal } from '@angular/core';
 import { FirestoreService } from '../../servizi/firestore.service';
+import { ActivatedRoute } from '@angular/router';
+import { BehaviorSubject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-tracking',
@@ -8,15 +10,33 @@ import { FirestoreService } from '../../servizi/firestore.service';
   templateUrl: './tracking.component.html',
   styleUrl: './tracking.component.css'
 })
-export class TrackingComponent implements OnInit {
+export class TrackingComponent implements OnInit, OnDestroy {
+  id = signal<string>('');
+  private idSubject = new BehaviorSubject<string>('');
+  private routeSub!: Subscription;
 
-  @Input({required: true}) giorno!: string;
-
-  constructor(private firestoreService: FirestoreService) {}
+  constructor(private firestoreService: FirestoreService, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.routeSub = this.route.params.subscribe((params) => {
+      this.id.set(params['id']);
+      this.idSubject.next(params['id']);
+    });
+
+    this.idSubject.subscribe((id) => {
+      this.getPercorso();
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.routeSub) {
+      this.routeSub.unsubscribe();
+    }
+  }
+
+  getPercorso(): void {
     this.firestoreService.getPercorsi().subscribe((percorsi) => {
-      console.log(percorsi);
+      console.log(percorsi.filter((percorso: any) => percorso.id === this.id())[0]);
     });
   }
 
