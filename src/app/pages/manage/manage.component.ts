2b3260455e5e06ce9eef01a02e82aaca41b899cd
gian <gianmarco.clu23@gmail.com>
2024-08-17 16:00:12 +0200

    feat(manage): added table

--- src/app/pages/manage/manage.component.ts
+++ src/app/pages/manage/manage.component.ts
@@ -2,19 +2,55 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { DropdownChangeEvent, DropdownModule } from 'primeng/dropdown';
 import { TabItem, tabItems } from '../../configs/tabItems';
+import { TableModule } from 'primeng/table';
+import { DatePipe, NgIf } from '@angular/common';
+import { Percorso } from '../../models/Percorso';
+import { FirestoreService } from '../../servizi/firestore.service';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-manage',
   standalone: true,
-  imports: [DropdownModule, FormsModule],
+  imports: [DropdownModule, FormsModule, NgIf, TableModule, DatePipe, ButtonModule],
   templateUrl: './manage.component.html',
   styleUrl: './manage.component.css'
 })
 export class ManageComponent {
   selectedDay!: TabItem;
   tabItems: TabItem[] = tabItems;
+  percorso: Percorso = {} as Percorso;
+
+  constructor(private firestoreService: FirestoreService) {}
 
   itemChanged(event: DropdownChangeEvent){
-    console.log(this.selectedDay)
+    this.firestoreService.getPercorsi().subscribe({
+      next: (percorsi) => {
+        this.percorso = percorsi.filter((percorso: any) => percorso.id === this.selectedDay.option)[0];
+        for (let i = 0; i < this.percorso.checkpoint.length; i++) {
+          if(this.percorso.checkpoint[i].orarioArrivoPrevisto){
+            //@ts-ignore
+            this.percorso.checkpoint[i].orarioArrivoPrevisto = this.percorso.checkpoint[i].orarioArrivoPrevisto.toDate();
+            //@ts-ignore
+            this.percorso.checkpoint[i].orarioArrivoEffettivo = this.percorso.checkpoint[i].orarioArrivoEffettivo.toDate();
+          }
+          if(this.percorso.checkpoint[i].orarioPartenzaPrevisto){
+            //@ts-ignore
+            this.percorso.checkpoint[i].orarioPartenzaPrevisto = this.percorso.checkpoint[i].orarioPartenzaPrevisto.toDate();
+            //@ts-ignore
+            this.percorso.checkpoint[i].orarioPartenzaEffettivo = this.percorso.checkpoint[i].orarioPartenzaEffettivo.toDate();
+          }
+          if(this.percorso.checkpoint[i].orarioPassaggioPrevisto){
+            //@ts-ignore
+            this.percorso.checkpoint[i].orarioPassaggioPrevisto = this.percorso.checkpoint[i].orarioPassaggioPrevisto.toDate();
+            //@ts-ignore
+            this.percorso.checkpoint[i].orarioPassaggioEffettivo = this.percorso.checkpoint[i].orarioPassaggioEffettivo.toDate();
+          }
+        }
+        console.log(this.percorso);
+      },
+      error: (error) => {
+        console.error(error);
+      }
+    })
   }
 }
