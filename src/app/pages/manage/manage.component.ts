8427420fbc07c1ae8eb18db72552276e2796584b
gian <gianmarco.clu23@gmail.com>
2024-08-17 14:58:14 +0200

    feat(manage): added dropdown in manage page

--- src/app/pages/manage/manage.component.ts
+++ src/app/pages/manage/manage.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { DropdownChangeEvent, DropdownModule } from 'primeng/dropdown';
+import { TabItem, tabItems } from '../../configs/tabItems';
 
 @Component({
   selector: 'app-manage',
   standalone: true,
-  imports: [],
+  imports: [DropdownModule, FormsModule],
   templateUrl: './manage.component.html',
   styleUrl: './manage.component.css'
 })
 export class ManageComponent {
+  selectedDay!: TabItem;
+  tabItems: TabItem[] = tabItems;
 
+  itemChanged(event: DropdownChangeEvent){
+    console.log(this.selectedDay)
+  }
 }
