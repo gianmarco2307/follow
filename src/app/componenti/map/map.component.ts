3578e66a478ea5282748636820020724067a2076
gian <gianmarco.clu23@gmail.com>
2024-08-18 14:56:22 +0200

    fix(map): fixed map when coordinates change

--- src/app/componenti/map/map.component.ts
+++ src/app/componenti/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { Feature, Map, View } from 'ol';
 import { Point } from 'ol/geom';
 import TileLayer from 'ol/layer/Tile';
@@ -16,54 +16,65 @@ import Style from 'ol/style/Style';
   templateUrl: './map.component.html',
   styleUrl: './map.component.css',
 })
-export class MapComponent implements OnInit {
-  map!: Map;
+export class MapComponent implements OnChanges {
+  map: Map | null = null;
+  marker: Feature | null = null;
+  vectorLayer: VectorLayer<VectorSource> | null = null;
 
   @Input({required: true}) coordinates: string = '';
 
-  ngOnInit(): void {
+  ngOnChanges(): void {
     // Coordinate di esempio (latitudine e longitudine)
     const coordinates = this.convertCoordinates(this.coordinates);
 
     const transformedCoordinates = fromLonLat(coordinates);
 
-    // Creazione del marker
-    const marker = new Feature({
-      geometry: new Point(transformedCoordinates),
-    });
+    if(this.map) {
+      if(this.marker){
+        this.marker.setGeometry(new Point(transformedCoordinates));
+      }
 
-    // Stile del marker
-    marker.setStyle(
-      new Style({
-        image: new Icon({
-          src: 'https://openlayers.org/en/latest/examples/data/icon.png',
-          anchor: [0.5, 1],
+      this.map.getView().setCenter(transformedCoordinates);
+      this.map.getView().setZoom(19);
+    } else {
+      // Creazione del marker
+      this.marker = new Feature({
+        geometry: new Point(transformedCoordinates),
+      });
+
+      // Stile del marker
+      this.marker.setStyle(
+        new Style({
+          image: new Icon({
+            src: 'https://openlayers.org/en/latest/examples/data/icon.png',
+            anchor: [0.5, 1],
+          }),
         }),
-      }),
-    );
+      );
 
-    // Layer vettoriale per il marker
-    const vectorLayer = new VectorLayer({
-      source: new VectorSource({
-        features: [marker],
-      }),
-    });
-
-    // Configurazione della mappa
-    this.map = new Map({
-      target: 'map',
-      layers: [
-        new TileLayer({
-          source: new OSM(),
+      // Layer vettoriale per il marker
+      this.vectorLayer = new VectorLayer({
+        source: new VectorSource({
+          features: [this.marker],
         }),
-        vectorLayer,
-      ],
-      view: new View({
-        center: transformedCoordinates,
-        zoom: 19,
-        projection: 'EPSG:3857',
-      }),
-    });
+      });
+
+      // Configurazione della mappa
+      this.map = new Map({
+        target: 'map',
+        layers: [
+          new TileLayer({
+            source: new OSM(),
+          }),
+          this.vectorLayer,
+        ],
+        view: new View({
+          center: transformedCoordinates,
+          zoom: 19,
+          projection: 'EPSG:3857',
+        }),
+      });
+    }
   }
 
   convertCoordinates(coordinates: string): number[] {
