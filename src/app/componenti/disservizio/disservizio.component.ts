837ad1d0cca82724daae63b07e16ead688269808
gian <gianmarco.clu23@gmail.com>
2024-08-20 21:06:29 +0200

    feat(disservizio): added edit disservizion functionality

--- src/app/componenti/disservizio/disservizio.component.ts
+++ src/app/componenti/disservizio/disservizio.component.ts
@@ -9,22 +9,26 @@ import { InputTextareaModule } from 'primeng/inputtextarea';
 import { FormsModule } from '@angular/forms';
 import { CheckboxModule } from 'primeng/checkbox';
 import { Disservizio } from '../../models/Disservizio';
+import { ListboxModule } from 'primeng/listbox';
 
 @Component({
   selector: 'app-disservizio',
   standalone: true,
-  imports: [ButtonModule, DialogModule, NgIf, DividerModule, InputTextareaModule, FormsModule, CheckboxModule],
+  imports: [ButtonModule, DialogModule, NgIf, DividerModule, InputTextareaModule, FormsModule, CheckboxModule, ListboxModule],
   templateUrl: './disservizio.component.html',
   styleUrl: './disservizio.component.css'
 })
 export class DisservizioComponent implements OnChanges {
   protected disserviziModalVisible: boolean = false;
   protected newDisservizioModalVisible: boolean = false;
+  protected editDisservizioModalVisible: boolean = false;
 
   protected disservizi: DisserviziCollection['disservizi'] = [];
 
   protected message = signal<string>('');
   protected activeDisservizio = signal<boolean>(true);
+
+  protected indexDisservizioToEdit = signal<number>(0);
 
   @Input({required: true}) id!: string;
 
@@ -32,11 +36,11 @@ export class DisservizioComponent implements OnChanges {
 
   ngOnChanges(changes: SimpleChanges): void {
     this.id = changes['id'].currentValue;
-    console.log(this.id);
+    // console.log(this.id);
     this.disserviziService.getDisservizi().subscribe((res) => {
       //@ts-ignore
       this.disservizi = res.find((disservizio) => disservizio.id === this.id).disservizi;
-      console.log(this.disservizi);
+      // console.log(this.disservizi);
     })
   }
 
@@ -48,7 +52,20 @@ export class DisservizioComponent implements OnChanges {
     this.newDisservizioModalVisible = true;
   }
 
+  onClickOpenEditDisservizioModal(index: number): void {
+    this.editDisservizioModalVisible = true;
+    this.indexDisservizioToEdit.set(index);
+    this.message.set(this.disservizi[index].descrizione);
+    this.activeDisservizio.set(this.disservizi[index].attivo);
+  }
+
   hidingNewDisservizioModal() {
+    this.message.set('');
+    this.activeDisservizio.set(true);
+  }
+
+  hidingEditDisservizioModal() {
+    this.indexDisservizioToEdit.set(0);
     this.message.set('');
     this.activeDisservizio.set(true);
   }
@@ -66,6 +83,21 @@ export class DisservizioComponent implements OnChanges {
     this.disserviziService.updateDisservizio(this.id, collection).subscribe({
       next: (res) => {
         this.newDisservizioModalVisible = false;
+      }
+    })
+  }
+
+  editDisservizio() {
+    let newDisservizi = [...this.disservizi];
+    newDisservizi[this.indexDisservizioToEdit()].descrizione = this.message();
+    newDisservizi[this.indexDisservizioToEdit()].attivo = this.activeDisservizio();
+    let collection: DisserviziCollection = {
+      id: this.id,
+      disservizi: newDisservizi
+    }
+    this.disserviziService.updateDisservizio(this.id, collection).subscribe({
+      next: (res) => {
+        this.editDisservizioModalVisible = false;
       }
     })
   }
